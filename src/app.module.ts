23bdeb22e44f8cc4a6297b21f848a5e265842acf
Guilherme <guilherme.bail@gauge.haus>
2024-09-24 13:52:09 -0300

    wip: iniciando projeto

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { PrismaService } from 'prisma/prisma.service';
+import { UserController } from './users/user.controller';
+import { UserRepository } from './users/user.repository';
+import { UserService } from './users/user.service';
+import { AccountController } from './accounts/account.controller';
+import { AccountRepository } from './accounts/account.repository';
+import { AccountService } from './accounts/account.service';
+import { TransactionController } from './transactions/transaction.controller';
+import { TransactionRepository } from './transactions/transaction.repository';
+import { TransactionService } from './transactions/transaction.service';
+
+@Module({
+  imports: [],
+  controllers: [AccountController, TransactionController, UserController],
+  providers: [
+    AccountService,
+    AccountRepository,
+    TransactionService,
+    TransactionRepository,
+    UserService,
+    UserRepository,
+    PrismaService,
+  ],
+})
+export class AppModule {}
