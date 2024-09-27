88ff5eecad01df27da9abf9a30db3f1d282e3ac6
Guilherme <guilherme.bail@gauge.haus>
2024-09-27 07:23:28 -0300

    wip : trocando porta

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,10 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3002',
+    origin: 'http://localhost:3000',
   });
 
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(3003);
 }
 bootstrap();
